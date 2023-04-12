6398dc42dbf82ae637ebd144655d62db18c0897d
stevegustason <steve.gustason@gmail.com>
2023-04-12 11:25:48 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Steven Gustason on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
