97c4d3c66919bdd87afb8ee2af7c10de5b3c5b1a
stevegustason <steve.gustason@gmail.com>
2023-04-14 11:21:51 -0500

    Day 3 - Learned about blurs, blending, animatableData, and AnimatablePair.

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -111,6 +111,8 @@ struct ContentView: View {
 }
 */
 
+/*
+// Day 2 content
 struct Flower: Shape {
     // How much to move this petal away from the center
     var petalOffset: Double = -20
@@ -210,7 +212,7 @@ struct ContentView: View {
             
             ColorCyclingCircle(amount: colorCycle)
                 .frame(width: 300, height: 300)
-            // drawingGroup tells SwiftUI it should render the contents of the view into an off-screen image before putting it back onto the screen as a single rendered output, which is significantly faster. Behind the scenes this is powered by Metal, which is Apple’s framework for working directly with the GPU for extremely fast graphics. 
+            // drawingGroup tells SwiftUI it should render the contents of the view into an off-screen image before putting it back onto the screen as a single rendered output, which is significantly faster. Behind the scenes this is powered by Metal, which is Apple’s framework for working directly with the GPU for extremely fast graphics.
                 .drawingGroup()
             
             
@@ -218,7 +220,230 @@ struct ContentView: View {
         }
     }
 }
+*/
 
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    
+    // Using animatableData allows us to animate something that otherwise would have just jumped from state to state - essentially it allows us to animate changes to shapes. Over the length of the animation, swift will set the animatableData property of our shape to the latest interpolated value until it reaches its target.
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
+}
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    // AnimatablePair allows us to animate two different values that are changing simultaneously
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+           AnimatablePair(Double(rows), Double(columns))
+        }
+
+        set {
+            // We can then set the first and second values here over time
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
+    }
+}
+
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: Double
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+
+        return a
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = Double(self.outerRadius)
+        let innerRadius = Double(self.innerRadius)
+        let distance = Double(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * Double.pi * outerRadius / Double(divisor)) * amount
+
+        var path = Path()
+
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+
+            x += rect.width / 2
+            y += rect.height / 2
+
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+
+        return path
+    }
+}
+
+struct ContentView: View {
+    @State private var insetAmount = 50.0
+    @State private var rows = 4
+    @State private var columns = 4
+    
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount = 1.0
+    @State private var hue = 0.6
+    
+    var body: some View {
+        /*
+         Image("Zelda")
+         // Color multiplly basically puts a colored filter on your image
+         .colorMultiply(.red)
+         */
+        
+        /*
+        // Screen inverts a color, performs a multiply, and then inverts them again to give a brighter image
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.green)
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+         
+         Image("Zelda")
+             .resizable()
+             .scaledToFit()
+             .frame(width: 200, height: 200)
+            // Saturation lets us control how much color is used in an image
+             .saturation(amount)
+             .blur(radius: (1 - amount) * 20)
+            
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
+         */
+        
+        /*
+        Trapezoid(insetAmount: insetAmount)
+                    .frame(width: 200, height: 100)
+                    .onTapGesture {
+                        withAnimation {
+                            insetAmount = Double.random(in: 10...90)
+                        }
+                    }
+         */
+        
+        /*
+        Checkerboard(rows: rows, columns: columns)
+                    .onTapGesture {
+                        withAnimation(.linear(duration: 3)) {
+                            rows = 8
+                            columns = 16
+                        }
+                    }
+         */
+        
+        VStack(spacing: 0) {
+                    Spacer()
+
+                    Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                        .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+                        .frame(width: 300, height: 300)
+
+                    Spacer()
+
+                    Group {
+                        Text("Inner radius: \(Int(innerRadius))")
+                        Slider(value: $innerRadius, in: 10...150, step: 1)
+                            .padding([.horizontal, .bottom])
+
+                        Text("Outer radius: \(Int(outerRadius))")
+                        Slider(value: $outerRadius, in: 10...150, step: 1)
+                            .padding([.horizontal, .bottom])
+
+                        Text("Distance: \(Int(distance))")
+                        Slider(value: $distance, in: 1...150, step: 1)
+                            .padding([.horizontal, .bottom])
+
+                        Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
+                        Slider(value: $amount)
+                            .padding([.horizontal, .bottom])
+
+                        Text("Color")
+                        Slider(value: $hue)
+                            .padding(.horizontal)
+                    }
+                }
+    }
+}
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
